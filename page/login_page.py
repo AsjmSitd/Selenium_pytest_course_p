91a4a888900321a95736b77a1698b61a37b1f38c
AsjmSitd <74098350+AsjmSitd@users.noreply.github.com>
2021-10-03 21:22:37 +0300

    Update and rename login_page.py to page/login_page.py

--- page/login_page.py
+++ page/login_page.py
@@ -1,15 +1,15 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
 
-link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+
 class LoginPage(BasePage):
-    def should_be_login_page(self, b):
-        self.should_be_login_url(b)
+    def should_be_login_page(self):
+        self.should_be_login_url()
         self.should_be_login_form()
         self.should_be_register_form()
 
-    def should_be_login_url(self, b):
-        assert b.current_url==link, f"Login url is not presented, current url is: '{b.current_url}'"# реализуйте проверку на корректный url адрес
+    def should_be_login_url(self):
+        assert "login" in self.browser.current_url, f"Login url is not presented, current url is: '{ self.browser.current_url}'"# реализуйте проверку на корректный url адрес
         assert True
 
     def should_be_login_form(self):
@@ -19,6 +19,3 @@ class LoginPage(BasePage):
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "register form is not presented"# реализуйте проверку, что есть форма регистрации на странице
         assert True
-
-
-
